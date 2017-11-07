99658f1bb770582068a53bfec58f39515353980a
Adrianne Alves <driah_as@hotmail.com>
2017-11-07 00:28:42 -0300

    Adding logical to compiled code in brainfuck

--- brainf_ck.py
+++ brainf_ck.py
@@ -16,9 +16,13 @@ def cGenerator(brainf_ck, o):
 	            "]": " \n}\n"}
 
 	c_file = """
-
 #include<stdio.h>
-
 int main(){
+char *tape = malloc(sizeof(char)*40000);
+char *pointer = &tape[0];
+"""
 
-}
+	source = brainf_ck.read()
+	for data in source:
+		if data in parseDictionary:
+			c_file = c_file + parseDictionary[data]
